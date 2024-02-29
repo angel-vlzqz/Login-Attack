cea871f497448dc1548501965ddf9074626ec133
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-02-29 14:27:24 -0700

    add session start to securepage

--- securepage.php
+++ securepage.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Secure Page</title>
-</head>
-<body>
+<?php
+session_start();
+?>
+
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <title>Secure Page</title>
+  </head>
+  <body>
   <h2>Secure page</h2>
   <p>This is a secure page that only logged in users should be able to view.</p>
   <p>If you somehow arrived here without logging in first, you will die in 7 days.</p>
-</body>
-</html>
+  </body>
+  </html>
